55bd3be73e0e57d2ab1e7ba4be80d4c41d1f22b0
747805 <sharmaharsh897@gmail.com>
2025-01-05 19:32:25 +0530

    added login button and user login form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,70 @@
-import React from 'react'
-import './Navbar.css';
-
+import React from "react";
+import { Link as RouterLink, useLocation } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
+import "./Navbar.css";
 import logoIcon from "../components/assets/gurudev.png";
 
 function Navbar() {
+  const location = useLocation();
+
+  const isHomePage = location.pathname === "/";
+
   return (
     <div>
       <nav className="navbar">
-      <div className="navbar-logo">
-      <img src={logoIcon} alt="Logo" className="logo-image" />
-      </div>
-      <ul className="navbar-links">
-        <li><a href="/index.html" target='_blank'>Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/contact">Contact Us</a></li>
-      </ul>
-    </nav>
+        <div className="navbar-logo">
+          <img src={logoIcon} alt="Logo" className="logo-image" />
+        </div>
+        <ul className="navbar-links">
+          <li>
+            {isHomePage ? (
+              <ScrollLink to="home" smooth={true} duration={100}>
+                Home
+              </ScrollLink>
+            ) : (
+              <RouterLink to="/">Home</RouterLink>
+            )}
+          </li>
+          <li>
+            {isHomePage ? (
+              <ScrollLink to="about" smooth={true} duration={100}>
+                About
+              </ScrollLink>
+            ) : (
+              <RouterLink to="/">About</RouterLink>
+            )}
+          </li>
+          <li>
+            {isHomePage ? (
+              <ScrollLink to="services" smooth={true} duration={100}>
+                Services
+              </ScrollLink>
+            ) : (
+              <RouterLink to="/">Services</RouterLink>
+            )}
+          </li>
+          <li>
+            {isHomePage ? (
+              <ScrollLink to="contact" smooth={true} duration={100}>
+                Contact Us
+              </ScrollLink>
+            ) : (
+              <RouterLink to="/">Contact Us</RouterLink>
+            )}
+          </li>
+          <li className="dropdown">
+            <a href="/" onClick={(e) => e.preventDefault()}>
+              Login
+            </a>
+            <div className="dropdown-menu">
+              <RouterLink to="/user-login">User Login</RouterLink>
+              <RouterLink to="/owner-login">Owner Login</RouterLink>
+            </div>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
---
+++ src/components/UserLogin.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./UserLogin.css"; // Import the CSS file
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Login Details:", { email, password });
+    // Add your login logic here
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h2 className="login-title">Login</h2>
+        <form onSubmit={handleLogin} className="login-form">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="login-input"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="login-input"
+            required
+          />
+          <button type="submit" className="login-button">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
