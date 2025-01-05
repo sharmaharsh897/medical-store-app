fc6e6c6f08cc783883f2e92dfd57824c600ebe25
747805 <sharmaharsh897@gmail.com>
2025-01-05 21:21:48 +0530

    Router setup to /home

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CarouselComponent from './components/CarouselComponent';
-import Footer from './components/Footer';
-import MapComponent from './components/MapComponent';
-import Navbar from './components/Navbar';
-import SearchSection from './components/SearchSection'; 
-import Testimonials from './components/Testimonials';
-import LoginForm from './components/UserLogin'; // Import your login form component
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import CarouselComponent from "./components/CarouselComponent";
+import Footer from "./components/Footer";
+import MapComponent from "./components/MapComponent";
+import Navbar from "./components/Navbar";
+import SearchSection from "./components/SearchSection";
+import Testimonials from "./components/Testimonials";
+import LoginForm from "./components/UserLogin"; // Import your login form component
 
 function App() {
   return (
-    <Router>
+    <Router basename="/medical-store-app">
       <div className="App">
         {/* Navbar remains visible on all routes */}
         <Navbar />
-        
+
         {/* Route Definitions */}
         <Routes>
-          {/* Default Home Route */}
-          <Route 
-            path="/" 
+          {/* Redirect from the root to /home */}
+          <Route path="/" element={<Navigate to="/home" />} />
+
+          {/* Home Route */}
+          <Route
+            path="/home"
             element={
               <>
                 <section id="home">
-        <SearchSection />
-        <CarouselComponent />
-      </section>
-      <section id="about">
-        <Testimonials />
-      </section>
-      <section id="contact">
-        <MapComponent />
-      </section>
+                  <SearchSection />
+                  <CarouselComponent />
+                </section>
+                <section id="about">
+                  <Testimonials />
+                </section>
+                <section id="contact">
+                  <MapComponent />
+                </section>
               </>
-            } 
+            }
           />
-          
+
           {/* Login Route */}
           <Route path="/user-login" element={<LoginForm />} />
         </Routes>
-        
+
         {/* Footer remains visible on all routes */}
         <Footer />
       </div>
