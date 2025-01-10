b87154a4974910eeca55cae17db94110fa35935f
747805 <sharmaharsh897@gmail.com>
2025-01-10 19:00:35 +0530

    react-router-dom fix to react-router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link as RouterLink, useLocation } from "react-router-dom";
+import { Link as RouterLink, useLocation } from "react-router";
 import { Link as ScrollLink } from "react-scroll";
 import "./Navbar.css";
 import logoIcon from "../components/assets/gurudev.png";
