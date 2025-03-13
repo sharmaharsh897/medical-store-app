1a4f2256386487290237f5e4e8d2d9d44db464d4
Harsh Sharma <sharmaharsh897@gmail.com>
2025-03-13 13:21:10 +0530

    Added quantity increase/decrease in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CartContext } from "../context/cartContext";
 import "./Cart.css";
 
 const Cart = () => {
   const { cart, removeFromCart } = useContext(CartContext);
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseQuantity = (item) => {
+    setQuantity(quantity + 1);
+  };
+
+  const decreaseQuantity = (item) => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
 
   return (
     <div className="cart-container">
@@ -17,8 +28,18 @@ const Cart = () => {
             <div className="cart-item-info">
               <p className="cart-item-name">{item.name}</p>
               <p className="cart-item-price">₹{item.price}</p>
+              <p>
+                Quantity: <button onClick={decreaseQuantity}>-</button>
+                <p>{quantity}</p>
+                <button onClick={increaseQuantity}>+</button>
+              </p>
             </div>
-            <button className="cart-remove-btn" onClick={()=>removeFromCart(item)}>Remove</button>
+            <button
+              className="cart-remove-btn"
+              onClick={() => removeFromCart(item)}
+            >
+              Remove
+            </button>
           </div>
         ))
       )}
