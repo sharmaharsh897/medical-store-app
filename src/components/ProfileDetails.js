f61e8e6cd17a2ce29727cfe11de77486d2506450
Harsh Sharma <sharmaharsh897@gmail.com>
2025-05-25 12:19:06 +0530

    added comment

--- src/components/ProfileDetails.js
+++ src/components/ProfileDetails.js
@@ -53,7 +53,7 @@ const UserAccountDetails = () => {
       </div>
     );
   }
-//comment added
+
   return (
     <div className="profile-details-container">
       <div className="profile-details-card">
