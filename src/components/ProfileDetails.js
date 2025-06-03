cceb5615929974125a8c95ea1ed90dab5644d9b3
Harsh Sharma <sharmaharsh897@gmail.com>
2025-06-03 11:07:01 +0530

    commit

--- src/components/ProfileDetails.js
+++ src/components/ProfileDetails.js
@@ -53,7 +53,7 @@ const UserAccountDetails = () => {
       </div>
     );
   }
-// commit1
+// commit
   return (
     <div className="profile-details-container">
       <div className="profile-details-card">
