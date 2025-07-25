2a97e00672d1654faac9ff6c584172817f219f78
Harsh Sharma <sharmaharsh897@gmail.com>
2025-07-25 10:39:15 +0530

    comment

--- src/components/MyOrders.js
+++ src/components/MyOrders.js
@@ -41,7 +41,7 @@ const MyOrders = () => {
     fetchOrdersAndAddresses();
   }, [token, sortOrder]);
 
-  //random comment1
+  //random comment
 
   const formatDate = (dateStr) => {
     const date = new Date(dateStr);
