eccf6e6e6570376722218d8aa9d171e1ae2399b1
Harsh Sharma <sharmaharsh897@gmail.com>
2025-09-23 19:34:54 +0530

    to create blueprint and copy mysql in render

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -2,10 +2,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_NAME || "medical",
+  port: process.env.DB_PORT || 3306, // allow custom port if provided
 });
 
 module.exports = db;
