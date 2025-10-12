426b0be859df14e0d392df5f455c52d19702d9ef
Harsh Sharma <sharmaharsh897@gmail.com>
2025-10-12 18:36:52 +0530

    Test console

--- backend/server/qrServer.js
+++ backend/server/qrServer.js
@@ -63,7 +63,6 @@ export function startQrServer({ port = 5001 } = {}) {
 
       const qrHost = `http://localhost:5001`;
 const html = generateInvoiceHTML(order, address, user, qrHost);
-//comment
 
 
       // Render to PNG using Puppeteer
